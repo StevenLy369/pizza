58ceb797ac27378668faab5419e492611ee4f8e5
Steven Ly <1stevenly92@gmail.com>
2020-04-10 09:49:30 -0700

    added basic logic for getting pizza price

--- js/script.js
+++ js/script.js
@@ -6,11 +6,51 @@ function Pizza(toppings, size){
 
 
 
+Pizza.prototype.addPizza=function(pizza){
+    this.pizzas.push(pizza);
+    console.log(pizza);
+}
+
+Pizza.prototype.price=function(){
+    var pizzaPrice = 10;
+
+    if (this.size == "xl"){
+        pizzaprice +=5;
+    }else if(this.size =="large"){
+        pizzaPrice +=3;
+    }else{
+        pizzaprice +=2;
+    }
+
+    if (this.toppings.length > 3){
+        pizzaPrice += 4;
+    }else if(this.toppings < 3){
+        pizzaprice += 6
+    }else{
+        break;
+    }
+
+
+
+
+}
+
+
+
+
+
 
 
 
 $(document).ready(function(){
 
+
+
+    
+    $("form").submit(function(event){
+        event.preventDefault();
+        console.log("hello");
+    })
 
 
 
